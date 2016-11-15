7411029e6d32850ce314f0f7adbcf708a2cea5d0
jky <yoongh97@naver.com>
2016-11-15 10:36:16 +0900

    make a main menu #4

--- app/src/main/java/pineapple/for_future/MainMenuActivity.java
+++ app/src/main/java/pineapple/for_future/MainMenuActivity.java
@@ -1,13 +1,41 @@
 package pineapple.for_future;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private long lastTimeBackPressed; //마지막으로 종료버튼을 누른 시간
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
+
+    @Override
+    public void onBackPressed(){
+
+        if(System.currentTimeMillis() - lastTimeBackPressed < 1500){
+            finish();
+            return ;
+        }
+        Toast.makeText(this,"뒤로 / 종료 버튼을 한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        lastTimeBackPressed = System.currentTimeMillis();
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.exit_button:
+                finish();
+        }
+    }
+
 }
