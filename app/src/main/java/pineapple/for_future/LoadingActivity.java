72e4821b5e772ff5db57459e0e2a99dc19df3bbc
jky <yoongh97@naver.com>
2016-11-16 10:04:39 +0900

    Make a Loading #9

---
+++ app/src/main/java/pineapple/for_future/LoadingActivity.java
@@ -0,0 +1,27 @@
+package pineapple.for_future;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by jk on 2016. 11. 16..
+ */
+
+public class LoadingActivity extends Activity {
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        try{
+            //2초 대기
+            Thread.sleep(2000);
+
+        }
+
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this,MainMenuActivity.class));
+        finish();
+    }
+}
