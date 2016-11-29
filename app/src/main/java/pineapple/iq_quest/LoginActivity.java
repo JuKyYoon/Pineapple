93706adf4ffff444fe7770b0f11b0177467e0248
waristo <wildstrip@naver.com>
2016-11-29 15:36:09 +0900

    login update #18

--- app/src/main/java/pineapple/iq_quest/LoginActivity.java
+++ app/src/main/java/pineapple/iq_quest/LoginActivity.java
@@ -3,8 +3,11 @@ package pineapple.iq_quest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import pineapple.for_future.R;
 
@@ -31,6 +34,38 @@ public class LoginActivity extends Activity{
         EditText password = (EditText)findViewById(R.id.password);
         String id = input_id.getText().toString();
         String pr = password.getText().toString();
+
+        //엔터키 제어
+        input_id.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId) {
+                    case EditorInfo.IME_ACTION_NEXT:
+                        // 다음 동작
+                        break;
+                    default:
+                        // 기본 엔터키 동작
+                        return false;
+                }
+                return true;
+            }
+        });
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId) {
+                    case EditorInfo.IME_ACTION_DONE:
+                        // 완료 동작
+                        break;
+                    default:
+                        // 기본 엔터키 동작
+                        return false;
+                }
+                return true;
+            }
+        });
+        //
         switch (view.getId())
         {
             case R.id.login:
